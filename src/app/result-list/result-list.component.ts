6b667dd1686d3dbe50dc56b6d4de7251edc5b871
Guest User <Guest@epicodus-94.local>
2019-04-15 18:44:50 -0700

    retrieve data from firebase

--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { OnInit, Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hike } from './../models/hike.model';
+import { HikeService } from '.././hike.service';
 
 @Component({
   selector: 'app-result-list',
   templateUrl: './result-list.component.html',
-  styleUrls: ['./result-list.component.css']
+  styleUrls: ['./result-list.component.css'],
+  providers: [HikeService]
 })
-export class ResultListComponent {
-  @Input() childHikeList: Hike[];
+export class ResultListComponent implements OnInit{
+  hikes;
+
+//  @Input() childHikeList: Hike[];
+
+  constructor(/*private router: Router, */private hikeService: HikeService){}
+  ngOnInit(){
+    this.hikes = this.hikeService.getHikes();
+    console.log(this.hikes);
+  }
 }
